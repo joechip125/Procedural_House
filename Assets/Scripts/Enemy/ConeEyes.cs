18773c93406e8f8be5f0924bb3c8cb45ce1f8e80
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-07-29 23:34:42 +0200

    Update ConeEyes.cs

--- Assets/Scripts/Enemy/ConeEyes.cs
+++ Assets/Scripts/Enemy/ConeEyes.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ConeEyes : MonoBehaviour
 {
     public List<Vector3> TracepointsList = new();
+
+    [Header("Cone Values")]
+    [Range(30f, 180f),SerializeField] private float angle;
+    [Range(1f, 60f),SerializeField] private float maxDistance;
     // Start is called before the first frame update
     
     void Start()
@@ -29,6 +35,51 @@ public class ConeEyes : MonoBehaviour
     {
         
     }
+
+    public enum ObjectTypes
+    {
+        Wall,
+        Player,
+        PickupPoint,
+        DropOffPoint,
+        DownedEnemy,
+        DownedPlayer
+    }
+
+    public ObjectInteractions objInt;
+    
+    [Flags]
+    public enum ObjectInteractions
+    {
+        None = 0,
+        CanPutDownItem = 1,
+        CanPickUpItem = 2,
+        CanDestroy = 4,
+        CanBuild = 8
+    };
+
+
+    public void AddAttributeToItem(bool addORemove, ObjectInteractions interaction)
+    {
+        if(interaction.HasFlag(interaction)) return;
+
+        if (addORemove)
+        {
+            
+        }
+        
+        if (!interaction.HasFlag(interaction))
+        {
+            objInt |= interaction;
+        }
+        
+        if(interaction is ObjectInteractions.CanBuild and interaction is ObjectInteractions.CanDestroy)
+    }
+    
+    public void RegisterWithObject()
+    {
+        
+    }
     
     public bool CheckVisibilityToPoint(Vector3 worldPoint)
     {
@@ -37,5 +88,13 @@ public class ConeEyes : MonoBehaviour
         
         var degreesToTarget =
             Vector3.Angle(transform.forward, directionToTarget);
+        
+        var withinArc = degreesToTarget < (angle / 2);
+        
+        var distanceToTarget = directionToTarget.magnitude;
+        
+        var rayDistance = Mathf.Min(maxDistance, distanceToTarget);
+
+        return withinArc;
     }
 }
