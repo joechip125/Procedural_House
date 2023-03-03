68e8db9c164ff711b28720cff742a836a032fd11
Mattias Lönnqvist <joechip125@gmail.com>
2023-03-03 14:57:21 +0100

    update

--- Assets/Scripts/Mesh/NewMeshTest.cs
+++ Assets/Scripts/Mesh/NewMeshTest.cs
@@ -26,51 +26,33 @@ public class NewMeshTest : MonoBehaviour
         mesh = gameObject.AddComponent<AdvancedMesh>();
         mesh.InstanceMesh();
         mesh.ApplyMaterial(aMaterial);
-        AddSomePoints();
         AddSomePoints2();
     }
-
-    public void NumberSetter()
-    {
-        
-    }
     
-    private void SetPoints(Directions direction, Vector2 size, Vector3 startPoint, bool backFront = false)
+    private void SetPoints(Vector3 direction, Vector3 size, Vector3 startPoint, bool backFront = false)
     {
         points.Clear();
-        
-        if (direction == Directions.Up)
-        {
-            arrayPoints[0] = startPoint;
-            arrayPoints[1] = startPoint + new Vector3(0, 0, size.x);
-            arrayPoints[2] = startPoint + new Vector3(0, size.y,0);
-            arrayPoints[3] = startPoint + new Vector3(0, size.y, size.x);
-            
-            if(backFront) points.Add(startPoint);
-            else points.Add(startPoint + new Vector3(0, size.y, size.x));
-            
-            points.Add(startPoint + new Vector3(0, 0, size.x));
-            points.Add(startPoint + new Vector3(0, size.y,0));
-            
-            if(!backFront) points.Add(startPoint);
-            else points.Add(startPoint + new Vector3(0, size.y, size.x));
-        }
-        else
-        {
-            //points.Add(startPoint);
-            //points.Add(startPoint + new Vector3(0, 0, size.x));
-            //points.Add(startPoint + new Vector3(size.x,0,0));
-            //points.Add(startPoint + new Vector3(size.x, 0, size.y));
-            
-            if(backFront) points.Add(startPoint);
-            else  points.Add(startPoint + new Vector3(size.x, 0, size.y));
-            
-            points.Add(startPoint + new Vector3(0, 0, size.x));
-            points.Add(startPoint + new Vector3(size.x,0,0));
 
-            if(!backFront) points.Add(startPoint);
-            else  points.Add(startPoint + new Vector3(size.x, 0, size.y));
+        if(backFront) points.Add(startPoint);
+        else points.Add(startPoint + Vector3.Scale(direction, size));
+
+        if (direction.z != 0)
+        {
+            points.Add(startPoint + new Vector3(0, 0, size.z));
         }
+        
+        if (direction.x != 0)
+        {
+            points.Add(startPoint + new Vector3(size.x, 0,0));
+        }
+
+        if (direction.y != 0)
+        {
+            points.Add(startPoint + new Vector3(0, size.y,0));
+        }
+
+        if(!backFront) points.Add(startPoint);
+        else points.Add(startPoint + Vector3.Scale(direction, size));
     }
 
     private void SetFloorCeil(Vector2 size, Vector3 startPoint)
@@ -89,19 +71,20 @@ public class NewMeshTest : MonoBehaviour
     private void AddPoints()
     {
         points.Clear();
-        
-        
     }
-    
-    private void AddSomePoints()
-    {
-        SetPoints(Directions.Down, new Vector2(100,100), transform.localPosition);
-        mesh.AddQuadWithPointList(points);
-    }
-    
+
     private void AddSomePoints2()
     {
-        SetPoints(Directions.Up, new Vector2(25,50),transform.localPosition + new Vector3(100,0,0));
+        var pos = transform.localPosition;
+        
+        SetPoints(new Vector3(1,0,1), new Vector3(100,100, 100), pos);
         mesh.AddQuadWithPointList(points);
+        
+        SetPoints(new Vector3(0,1,1), new Vector3(100,100, 100), pos, true);
+        mesh.AddQuadWithPointList(points);
+        
+        SetPoints(new Vector3(1,1,0), new Vector3(100,100, 100), pos);
+        mesh.AddQuadWithPointList(points);
+        
     }
 }
