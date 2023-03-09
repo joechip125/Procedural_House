4a3e1889d530fb2c932fdaf4a940f4685309704a
Mattias Lönnqvist <joechip125@gmail.com>
2023-03-09 18:22:12 +0100

    update

--- Assets/Scripts/Mesh/Staircase.cs
+++ Assets/Scripts/Mesh/Staircase.cs
@@ -9,6 +9,10 @@ public class Staircase : MonoBehaviour
 {
     private AdvancedMesh mesh;
     [SerializeField] private Material mat;
+    [Range(2, 40)]public float height;
+    [Range(2, 100)]public float width;
+    
+    
     private void Awake()
     {
         if (!Application.isEditor) return;
@@ -20,18 +24,20 @@ public class Staircase : MonoBehaviour
 
     private void Try()
     {
-        var add = new Vector3(0, 30, 0);
+        var add = new Vector3(0, height, 0);
         var t1 = transform.position;
         var t2 = t1 + add;
-        var t3 = t1 + new Vector3(100,0,0);
+        var t3 = t1 + new Vector3(width,0,0);
         var t4 = t3 + add;
         mesh.AddQuad(t1, t2, t3, t4);
+
+        var secS = t1 + add;
         
-        var add2 = new Vector3(0, 30, 0);
-        var t5 = transform.position;
-        var t6 = t1 + add;
-        var t7 = t1 + new Vector3(100,0,0);
-        var t8 = t3 + add;
-        mesh.AddQuad(t1, t2, t3, t4);
+        var add2 = new Vector3(0, 0, -20);
+        var t5 =secS;
+        var t6 = t5 + add2;
+        var t7 = t5 + new Vector3(width,0,0);
+        var t8 = t7 + add2;
+        mesh.AddQuad(t5, t6, t7, t8);
     }
 }
