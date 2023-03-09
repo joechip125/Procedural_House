b5d7f91fa12c41c3b1c08c0c3a8f1c45bb599293
Mattias Lönnqvist <joechip125@gmail.com>
2023-03-09 19:10:29 +0100

    Update Staircase.cs

--- Assets/Scripts/Mesh/Staircase.cs
+++ Assets/Scripts/Mesh/Staircase.cs
@@ -22,13 +22,29 @@ public class Staircase : MonoBehaviour
         Try();
     }
 
+    private void AddPanel()
+    {
+        
+    }
+    
     private void Try()
     {
+        var start = transform.position;
+        var addH =new Vector3(0, height, 0);
+        var addW = new Vector3(width,0,0);
+        var addL = new Vector3(0,0,-20);
+        
+        
+        for (int i = 0; i < 8; i++)
+        {
+            var v1 = start;
+        }
+
         var add = new Vector3(0, height, 0);
         var t1 = transform.position;
-        var t2 = t1 + add;
-        var t3 = t1 + new Vector3(width,0,0);
-        var t4 = t3 + add;
+        var t2 = t1 + addH;
+        var t3 = t1 + addW;
+        var t4 = t3 + addH;
         mesh.AddQuad(t1, t2, t3, t4);
 
         var secS = t1 + add;
@@ -36,7 +52,7 @@ public class Staircase : MonoBehaviour
         var add2 = new Vector3(0, 0, -20);
         var t5 =secS;
         var t6 = t5 + add2;
-        var t7 = t5 + new Vector3(width,0,0);
+        var t7 = t5 + addL;
         var t8 = t7 + add2;
         mesh.AddQuad(t5, t6, t7, t8);
     }
