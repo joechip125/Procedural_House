b8380e9dca60eca43a8549d6e2ea65b24e991cea
Mattias Lönnqvist <joechip125@gmail.com>
2023-03-10 14:58:47 +0100

    Update ExampleWindow.cs

--- Assets/Scripts/Editor/ExampleWindow.cs
+++ Assets/Scripts/Editor/ExampleWindow.cs
@@ -1,17 +1,33 @@
 using System;
 using UnityEditor;
 using UnityEngine;
+using Object = System.Object;
 
 public class ExampleWindow : EditorWindow
 {
-    [MenuItem("My Tools/ Open Example Window")]
+    public Color color;
+    
+    [MenuItem("My Tools/ Colorize")]
     public static void ShowWindow()
     {
-        GetWindow<ExampleWindow>();
+        GetWindow<ExampleWindow>("Colorize");
     }
     
     private void OnGUI()
     {
-        
+        GUILayout.Label("Color the selected objects", EditorStyles.boldLabel);
+
+        color = EditorGUILayout.ColorField("The Color", color);
+        if (GUILayout.Button("Colorize"))
+        {
+            foreach (var g in Selection.gameObjects)
+            {
+                var rend = g.GetComponent<Renderer>();
+                
+                if(Equals(rend, null)) continue;
+
+                rend.sharedMaterial.color = color;
+            }
+        }
     }
 }
