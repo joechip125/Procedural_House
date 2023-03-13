c62482a719be5313d61cbe789478c45ffd8e52de
Mattias Lönnqvist <joechip125@gmail.com>
2023-03-13 15:15:18 +0100

    Update TestWall.cs

--- Assets/TestWall.cs
+++ Assets/TestWall.cs
@@ -1,21 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+
+[Serializable]
+public class Doorway
+{
+    public float theStart;
+    [Range(10, 500)]public float sizeX;
+    [Range(10, 500)]public float sizeY;
+    public Doorway(float start)
+    {
+        theStart = start;
+    }
+}
 
 public class TestWall : MonoBehaviour
 {
-
     [Range(10, 500)]public float sizeX;
     [Range(10, 500)]public float sizeY;
-    
+    public List<Doorway> doors = new ();
+
+    public void AddDoor()
+    {
+        
+    }
     
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
@@ -24,7 +40,23 @@ public class TestWall : MonoBehaviour
     private void OnDrawGizmos()
     {
         var pos = transform.position;
+        var max = pos + new Vector3(sizeX, sizeY, 0);
         
         Gizmos.color = Color.red;
+        Gizmos.DrawLine(pos, pos + new Vector3(0, sizeY,0));
+        Gizmos.DrawLine(pos, pos + new Vector3(sizeX, 0,0));
+        
+        Gizmos.DrawLine(pos+ new Vector3(0, sizeY,0), pos+ new Vector3(0, sizeY,0) + new Vector3(sizeX, 0,0));
+        
+        Gizmos.DrawLine(pos+ new Vector3(sizeX, 0,0), pos + new Vector3(sizeX, 0,0) + new Vector3(0, sizeY,0));
+
+        for (int i = 0; i < doors.Count; i++)
+        {
+            var start = pos + new Vector3(doors[i].theStart, 0, 0);
+            var theX = doors[i].sizeX;
+            var theY = doors[i].sizeY;
+            Gizmos.DrawLine(start, start + new Vector3(0, theY, 0));
+        }
+        
     }
 }
