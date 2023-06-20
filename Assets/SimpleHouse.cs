a1240cd6bc9e2c3990512bf2ace8f9cd563872f0
Mattias Lönnqvist <joechip125@gmail.com>
2023-06-20 19:34:03 +0200

    update

--- Assets/SimpleHouse.cs
+++ Assets/SimpleHouse.cs
@@ -4,10 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class MeshHolder
+public class RoomHolder
 {
-    public NewAdvancedMesh mesh;
-    public Transform gameObjectTransform;
+    public SingleRoom theRoom;
 }
 
 public class SimpleHouse : MonoBehaviour
@@ -15,27 +14,21 @@ public class SimpleHouse : MonoBehaviour
     private List<NewAdvancedMesh> meshes = new();
     [SerializeField] private List<GameObject> roomTiles = new();
     private Vector3 startSize;
+    private List<RoomHolder> rooms = new();
     
 
     void Start()
     {
         var center = new Vector3();
-        AddFloor();
-        AddWall();
+        SpawnRoom();
     }
 
-    private void AddCallback(Vector3 start, Vector3 direction)
-    {
-        var add = direction * 155f;
-       
-        var newStart = start + add;
-        meshes.Add(Instantiate(roomTiles[0], newStart, 
-            Quaternion.identity, transform).GetComponent<NewAdvancedMesh>());
-        
-        var temp = (NewAdvancedMesh_Floor) meshes[^1];
-        temp.SetValuesAndActivate(100, 4,4);
-        AddWall();
 
+    private void SpawnRoom()
+    {
+        var temp = Instantiate(roomTiles[0], transform)
+            .GetComponent<SingleRoom>();
+        rooms.Add(new RoomHolder(){ theRoom = temp});
     }
 
     private void AddWall()
@@ -67,7 +60,6 @@ public class SimpleHouse : MonoBehaviour
     {
         meshes.Add(Instantiate(roomTiles[0], transform).GetComponent<NewAdvancedMesh>());
         var temp = (NewAdvancedMesh_Floor)meshes[^1];
-        temp.Callback = AddCallback;
         temp.SetValuesAndActivate(100, 5,5);
         startSize = new Vector3(500,100,500);
     }
