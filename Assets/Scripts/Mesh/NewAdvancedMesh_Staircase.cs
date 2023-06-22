2144cf087c468aa0df101fcc3272596d7c3cc2fa
Mattias Lönnqvist <joechip125@gmail.com>
2023-06-22 18:38:43 +0200

    update

--- Assets/Scripts/Mesh/NewAdvancedMesh_Staircase.cs
+++ Assets/Scripts/Mesh/NewAdvancedMesh_Staircase.cs
@@ -15,15 +15,22 @@ public class NewAdvancedMesh_Staircase : NewAdvancedMesh
         var stepDepth2 = direction * stepDepth / 2;
         var first = new Vector2(stepWidth, stepHeight);
         var second = new Vector2(stepWidth, stepDepth);
+        var start = stepDepth * numSteps;
+        var third = new Vector2(start, stepHeight);
 
         var pos = new Vector3(0, size.y / 2, 0);
         
         for (int i = 0; i < numSteps; i++)
         {
             SimplePanel(pos, -direction, first);
+            var aCross = Vector3.Cross(Vector3.up, direction);
+            SimplePanel(pos + aCross * stepWidth / 2  + direction * start / 2, aCross, third);
+            SimplePanel(pos -aCross * stepWidth / 2  + direction * start / 2, -aCross, third);
             pos += Vector3.up * stepHeight / 2 + stepDepth2;
             SimplePanel(pos, Vector3.up, second);
             pos += Vector3.up * stepHeight / 2 + stepDepth2;
+            start -= stepDepth;
+            third.x -= stepDepth;
         }
     }
 }
