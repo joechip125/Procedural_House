284396bf6728f90236d855f0d8ee417bbb17c0a4
Mattias Lönnqvist <joechip125@gmail.com>
2023-09-04 17:24:50 +0200

    Update NewAdvancedMesh_Wall.cs

--- Assets/Scripts/Mesh/NewAdvancedMesh_Wall.cs
+++ Assets/Scripts/Mesh/NewAdvancedMesh_Wall.cs
@@ -394,18 +394,18 @@ public class NewAdvancedMesh_Wall : NewAdvancedMesh
         var size = new Vector3(200, 200);
         var size2 = new Vector3(100, 100);
         var corner = new Vector3(15, 15);
-        AddSegment(pos, size, corner, Vector3.zero);
+       // AddSegment(pos, size, corner, Vector3.zero);
         
-       // SquareSegment(pos, size, corner, Vector3.zero);
+        SquareSegment(pos, size, corner, Vector3.right);
         var dir = -(Vector3.right + Vector3.back);
         
         var amount = ((size.x + corner.x) / 2) + ((size2.x + corner.x) / 2);
         var next = pos + Vector3.right * amount;
-       // SquareSegment(next, size2, corner, Vector3.right);
+        SquareSegment(next, size2, corner, Vector3.right * 2);
         var angle= Quaternion.AngleAxis(45, Vector3.up) *dir;
         var angle2= Quaternion.AngleAxis(-45, Vector3.up) *dir;
         
-       // DrawCorners(pos);
+        DrawCorners(pos);
     }
 
     private void AddSegment(Vector3 pos, Vector3 roomS, Vector3 cornerS, Vector3 currIndex)
