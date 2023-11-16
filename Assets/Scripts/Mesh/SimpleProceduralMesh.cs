0c63dc6faa31c5920bec847e17b0f3ef1a98e17c
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-16 15:24:42 +0100

    update

--- Assets/Scripts/Mesh/SimpleProceduralMesh.cs
+++ Assets/Scripts/Mesh/SimpleProceduralMesh.cs
@@ -14,22 +14,31 @@ public class SimpleProceduralMesh : MonoBehaviour
         
         mesh.vertices = new Vector3[] 
         {
-            Vector3.zero, Vector3.right, Vector3.up
+            Vector3.zero, Vector3.right, Vector3.up, new Vector3(1f, 1f)
         };
 
         mesh.triangles = new int[] 
         {
-            0, 1, 2
+            0, 1, 2,1,3,2
         };
         
         mesh.normals = new Vector3[] 
         {
-            Vector3.back, Vector3.back, Vector3.back
+            Vector3.back, Vector3.back, Vector3.back, Vector3.back
         };
         
         mesh.uv = new Vector2[] 
         {
-            Vector2.zero, Vector2.right, Vector2.up
+            Vector2.zero, Vector2.right, Vector2.up, Vector2.one
+        };
+
+        
+        mesh.tangents = new Vector4[] 
+        {
+            new Vector4(1f, 0f, 0f, -1f),
+            new Vector4(1f, 0f, 0f, -1f),
+            new Vector4(1f, 0f, 0f, -1f),
+            new Vector4(1f, 0f, 0f, -1f)
         };
     }
 }
