f9affcdffcbfe8311c0426c48b83f3fa206abd54
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-16 14:56:27 +0100

    Update SimpleProceduralMesh.cs

--- Assets/Scripts/Mesh/SimpleProceduralMesh.cs
+++ Assets/Scripts/Mesh/SimpleProceduralMesh.cs
@@ -11,5 +11,20 @@ public class SimpleProceduralMesh : MonoBehaviour
             name = "Procedural Mesh"
         };
         GetComponent<MeshFilter>().mesh = mesh;
+        
+        mesh.vertices = new Vector3[] 
+        {
+            Vector3.zero, Vector3.right, Vector3.up
+        };
+
+        mesh.triangles = new int[] 
+        {
+            0, 1, 2
+        };
+        
+        mesh.normals = new Vector3[] 
+        {
+            Vector3.back, Vector3.back, Vector3.back
+        };
     }
 }
