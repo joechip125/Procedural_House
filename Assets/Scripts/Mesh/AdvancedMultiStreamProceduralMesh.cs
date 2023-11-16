8ffe0ec2a84e380ad68367710cfa635f1a2fd284
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-16 17:03:08 +0100

    Update AdvancedMultiStreamProceduralMesh.cs

--- Assets/Scripts/Mesh/AdvancedMultiStreamProceduralMesh.cs
+++ Assets/Scripts/Mesh/AdvancedMultiStreamProceduralMesh.cs
@@ -1,7 +1,8 @@
 ﻿using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
-
+using Unity.Mathematics;
+using static Unity.Mathematics.math;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
@@ -10,13 +11,22 @@ public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
     {
         int vertexAttributeCount = 4;
         int vertexCount = 4;
+        int triangleIndexCount = 6;
+        
         Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1);
         Mesh.MeshData meshData = meshDataArray[0];
         
-        
         var vertexAttributes = new NativeArray<VertexAttributeDescriptor>(
-            vertexAttributeCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory
-        );
+            vertexAttributeCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+        
+        meshData.SetIndexBufferParams(triangleIndexCount, IndexFormat.UInt32);
+        NativeArray<uint> triangleIndices = meshData.GetIndexData<uint>();
+        triangleIndices[0] = 0;
+        triangleIndices[1] = 2;
+        triangleIndices[2] = 1;
+        triangleIndices[3] = 1;
+        triangleIndices[4] = 2;
+        triangleIndices[5] = 3;
         
         var mesh = new Mesh 
         {
@@ -36,5 +46,23 @@ public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
         
         meshData.SetVertexBufferParams(vertexCount, vertexAttributes);
         vertexAttributes.Dispose();
+        
+        NativeArray<float3> positions = meshData.GetVertexData<float3>();
+        positions[0] = 0f;
+        positions[1] = right();
+        positions[2] = up();
+        positions[3] = float3(1f, 1f, 0f);
+        
+        NativeArray<float3> normals = meshData.GetVertexData<float3>(1);
+        normals[0] = normals[1] = normals[2] = normals[3] = back();
+
+        NativeArray<float4> tangents = meshData.GetVertexData<float4>(2);
+        tangents[0] = tangents[1] = tangents[2] = tangents[3] = float4(1f, 0f, 0f, -1f);
+
+        NativeArray<float2> texCoords = meshData.GetVertexData<float2>(3);
+        texCoords[0] = 0f;
+        texCoords[1] = float2(1f, 0f);
+        texCoords[2] = float2(0f, 1f);
+        texCoords[3] = 1f;
     }
 }
