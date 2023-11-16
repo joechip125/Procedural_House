a0dc92352b8d5515fe5915a40cc6f82eab000bf6
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-16 19:00:06 +0100

    update

---
+++ Assets/Scripts/Mesh/AdvancedSingleStreamProceduralMesh.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using Unity.Mathematics;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class AdvancedSingleStreamProceduralMesh : MonoBehaviour
+{
+    
+    [StructLayout(LayoutKind.Sequential)]
+    struct Vertex 
+    {
+        public float3 position, normal;
+        public half4 tangent;
+        public half2 texCoord0;
+    }
+}
