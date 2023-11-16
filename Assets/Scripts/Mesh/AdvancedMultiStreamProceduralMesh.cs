ad11565b50cae9c728a6b75a0bea776d4c02a9e1
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-16 16:56:11 +0100

    Update AdvancedMultiStreamProceduralMesh.cs

--- Assets/Scripts/Mesh/AdvancedMultiStreamProceduralMesh.cs
+++ Assets/Scripts/Mesh/AdvancedMultiStreamProceduralMesh.cs
@@ -9,12 +9,13 @@ public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
     void OnEnable ()
     {
         int vertexAttributeCount = 4;
+        int vertexCount = 4;
         Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1);
         Mesh.MeshData meshData = meshDataArray[0];
         
         
         var vertexAttributes = new NativeArray<VertexAttributeDescriptor>(
-            vertexAttributeCount, Allocator.Temp
+            vertexAttributeCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory
         );
         
         var mesh = new Mesh 
@@ -24,5 +25,16 @@ public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
 
         Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
         GetComponent<MeshFilter>().mesh = mesh;
+        
+        vertexAttributes[0] = new VertexAttributeDescriptor(dimension: 3);
+        vertexAttributes[1] = new VertexAttributeDescriptor
+            (VertexAttribute.Normal, dimension: 3, stream: 1);
+        vertexAttributes[2] = new VertexAttributeDescriptor
+            (VertexAttribute.Tangent, dimension: 4, stream: 2);
+        vertexAttributes[3] = new VertexAttributeDescriptor
+        (VertexAttribute.TexCoord0, dimension: 2, stream: 3);
+        
+        meshData.SetVertexBufferParams(vertexCount, vertexAttributes);
+        vertexAttributes.Dispose();
     }
 }
