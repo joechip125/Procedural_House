74a4a675beea015bda6ec0bd3b1967499c2a47cd
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-16 16:48:17 +0100

    update

---
+++ Assets/Scripts/Mesh/AdvancedMultiStreamProceduralMesh.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
+{
+    void OnEnable ()
+    {
+        int vertexAttributeCount = 4;
+        Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1);
+        Mesh.MeshData meshData = meshDataArray[0];
+        
+        
+        var vertexAttributes = new NativeArray<VertexAttributeDescriptor>(
+            vertexAttributeCount, Allocator.Temp
+        );
+        
+        var mesh = new Mesh 
+        {
+            name = "Procedural Mesh"
+        };
+
+        Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+}
