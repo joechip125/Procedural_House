cf531d4a3915a1140f3a2dc2ea99bcf8b3dfd1c1
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-16 18:55:37 +0100

    Update AdvancedMultiStreamProceduralMesh.cs

--- Assets/Scripts/Mesh/AdvancedMultiStreamProceduralMesh.cs
+++ Assets/Scripts/Mesh/AdvancedMultiStreamProceduralMesh.cs
@@ -27,13 +27,21 @@ public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
         triangleIndices[3] = 1;
         triangleIndices[4] = 2;
         triangleIndices[5] = 3;
-
+        
+        var bounds = new Bounds(new Vector3(0.5f, 0.5f), new Vector3(1f, 1f));
+        
         meshData.subMeshCount = 1;
-        meshData.SetSubMesh(0, new SubMeshDescriptor(0, triangleIndexCount));
+        meshData.SetSubMesh(0, new SubMeshDescriptor(0, triangleIndexCount)
+        {
+            bounds = bounds,
+            vertexCount = vertexCount,
+            
+        }, MeshUpdateFlags.DontRecalculateBounds);
         
         var mesh = new Mesh 
         {
-            name = "Procedural Mesh"
+            name = "Procedural Mesh",
+            bounds = bounds
         };
 
         Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
@@ -43,9 +51,9 @@ public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
         vertexAttributes[1] = new VertexAttributeDescriptor
             (VertexAttribute.Normal, dimension: 3, stream: 1);
         vertexAttributes[2] = new VertexAttributeDescriptor
-            (VertexAttribute.Tangent, dimension: 4, stream: 2);
+            (VertexAttribute.Tangent, VertexAttributeFormat.Float16, 4, 2);
         vertexAttributes[3] = new VertexAttributeDescriptor
-        (VertexAttribute.TexCoord0, dimension: 2, stream: 3);
+        (VertexAttribute.TexCoord0, VertexAttributeFormat.Float16, 2, 3);
         
         meshData.SetVertexBufferParams(vertexCount, vertexAttributes);
         vertexAttributes.Dispose();
@@ -59,13 +67,15 @@ public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
         NativeArray<float3> normals = meshData.GetVertexData<float3>(1);
         normals[0] = normals[1] = normals[2] = normals[3] = back();
 
-        NativeArray<float4> tangents = meshData.GetVertexData<float4>(2);
-        tangents[0] = tangents[1] = tangents[2] = tangents[3] = float4(1f, 0f, 0f, -1f);
+        half h0 = half(0f), h1 = half(1f);
+        
+        NativeArray<half4> tangents = meshData.GetVertexData<half4>(2);
+        tangents[0] = tangents[1] = tangents[2] = tangents[3] = half4(h1, h0, h0, half(-1f));
 
-        NativeArray<float2> texCoords = meshData.GetVertexData<float2>(3);
-        texCoords[0] = 0f;
-        texCoords[1] = float2(1f, 0f);
-        texCoords[2] = float2(0f, 1f);
-        texCoords[3] = 1f;
+        NativeArray<half2> texCoords = meshData.GetVertexData<half2>(3);
+        texCoords[0] = h0;
+        texCoords[1] = half2(h1, h0);
+        texCoords[2] = half2(h0, h1);
+        texCoords[3] = h1;
     }
 }
