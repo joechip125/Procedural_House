a3ddd6df219b94ef4436eca38b0dce750cb0c5dc
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-17 17:19:24 +0100

    update

--- Assets/Scripts/Mesh/Procedural/MeshJob.cs
+++ Assets/Scripts/Mesh/Procedural/MeshJob.cs
@@ -1,6 +1,8 @@
 ﻿
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Jobs;
+using UnityEngine;
 
 [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
 public struct MeshJob<G, S> : IJobFor
@@ -8,7 +10,17 @@ public struct MeshJob<G, S> : IJobFor
     where S : struct, IMeshStreams
 {
     G generator;
+    
+    [WriteOnly]
     S streams;
 
     public void Execute (int i) => generator.Execute(i, streams);
+
+    public static JobHandle ScheduleParallel(Mesh.MeshData meshData, JobHandle dependency)
+    {
+        var job = new MeshJob<G, S>();
+        job.streams.Setup(meshData, job.generator.VertexCount, job.generator.IndexCount);
+        
+        return job.ScheduleParallel(job.generator.JobLength, 1, dependency);
+    }
 }
---
+++ Assets/Scripts/Mesh/Procedural/ProceduralMesh.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralMesh : MonoBehaviour
+{
+    Mesh mesh;
+
+    void Awake () 
+    {
+        mesh = new Mesh 
+        {
+            name = "Procedural Mesh"
+        };
+        GenerateMesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    void GenerateMesh()
+    {
+        Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1);
+        Mesh.MeshData meshData = meshDataArray[0];
+
+        MeshJob<SquareGrid, SingleStream>.ScheduleParallel(meshData, default).Complete();
+
+        Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
+    }
+}
