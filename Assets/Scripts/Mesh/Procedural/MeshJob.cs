94a390420cd3b7b9c5f12640828385ac614d7747
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-17 17:09:00 +0100

    update

---
+++ Assets/Scripts/Mesh/Procedural/MeshJob.cs
@@ -0,0 +1,14 @@
+﻿
+using Unity.Burst;
+using Unity.Jobs;
+
+[BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+public struct MeshJob<G, S> : IJobFor
+    where G : struct, IMeshGenerator
+    where S : struct, IMeshStreams
+{
+    G generator;
+    S streams;
+
+    public void Execute (int i) => generator.Execute(i, streams);
+}
