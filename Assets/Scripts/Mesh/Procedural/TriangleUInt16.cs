d59fad98f58f34bc5b2f2c40ba3f781bfbf40597
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-20 15:06:21 +0100

    update

---
+++ Assets/Scripts/Mesh/Procedural/TriangleUInt16.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Runtime.InteropServices;
+using Unity.Mathematics;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct TriangleUInt16
+{
+    public ushort a, b, c;
+
+    public static implicit operator TriangleUInt16 (int3 t) => new TriangleUInt16 
+    {
+        a = (ushort)t.x,
+        b = (ushort)t.y,
+        c = (ushort)t.z
+    };
+}
