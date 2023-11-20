fc15814392f9105942f26819c88b4ade66d88587
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-20 14:50:31 +0100

    Update SquareGrid.cs

--- Assets/Scripts/Mesh/Procedural/SquareGrid.cs
+++ Assets/Scripts/Mesh/Procedural/SquareGrid.cs
@@ -6,10 +6,12 @@ public struct SquareGrid : IMeshGenerator
 {
     public void Execute<S>(int i, S streams) where S : struct, IMeshStreams
     {
-        
+        var vertex = new Vertex();
+        vertex.normal.z = -1f;
+        vertex.tangent.xw = float2(1f, -1f);
     }
 
-    public int VertexCount { get; }
+    public int VertexCount => 4;
     public int IndexCount { get; }
     public int JobLength => 1;
 }
