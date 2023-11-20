215c66c2e12143722e4069dd7495e389443743c1
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-20 18:14:26 +0100

    Update SquareGrid.cs

--- Assets/Scripts/Mesh/Procedural/SquareGrid.cs
+++ Assets/Scripts/Mesh/Procedural/SquareGrid.cs
@@ -6,23 +6,25 @@ public struct SquareGrid : IMeshGenerator
 {
     public void Execute<S>(int i, S streams) where S : struct, IMeshStreams
     {
+        int vi = 4 * i, ti = 2 * i;
+        
         var vertex = new Vertex();
         vertex.normal.z = -1f;
         vertex.tangent.xw = float2(1f, -1f);
         
-        streams.SetVertex(0, vertex);
+        streams.SetVertex(vi+ 0, vertex);
         
         vertex.position = right();
         vertex.texCoord0 = float2(1f, 0f);
-        streams.SetVertex(1, vertex);
+        streams.SetVertex(vi +1, vertex);
 
         vertex.position = up();
         vertex.texCoord0 = float2(0f, 1f);
-        streams.SetVertex(2, vertex);
+        streams.SetVertex(vi +2, vertex);
 
         vertex.position = float3(1f, 1f, 0f);
         vertex.texCoord0 = 1f;
-        streams.SetVertex(3, vertex);
+        streams.SetVertex(vi +3, vertex);
         
         streams.SetTriangle(0, int3(0, 2, 1));
         streams.SetTriangle(1, int3(1, 2, 3));
