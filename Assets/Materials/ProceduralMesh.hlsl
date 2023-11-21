57d896350e8787ac705900ad8eb0229914050bd2
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-21 20:23:50 +0100

    update

--- Assets/Materials/ProceduralMesh.hlsl
+++ Assets/Materials/ProceduralMesh.hlsl
@@ -3,7 +3,7 @@
 void Ripple_float (
 float3 PositionIn, float3 Origin,
 float Period, float Speed, float Amplitude,
-out float3 PositionOut
+out float3 PositionOut, out float3 NormalOut, out float3 TangentOut
 ) {
     float3 p = PositionIn - Origin;
     static const float PI = 3.14159265f;
@@ -11,4 +11,8 @@ out float3 PositionOut
     float f = 2.0 * PI * Period * (d - Speed * _Time.y);
 	
     PositionOut = PositionIn + float3(0.0, Amplitude * sin(f), 0.0);
+    float2 derivatives = (2.0 * PI * Amplitude * Period * cos(f) / max(d, 0.0001)) * p.xz;
+
+    TangentOut = float3(1.0, derivatives.x, 0.0);
+    NormalOut = cross(float3(0.0, derivatives.y, 1.0), TangentOut);
 }
