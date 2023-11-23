e5041adf9387daca4cc46350281a7443fac49839
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-23 18:15:27 +0100

    update

--- Assets/Materials/ProceduralMesh.hlsl
+++ Assets/Materials/ProceduralMesh.hlsl
@@ -1,10 +1,9 @@
 ﻿#include <UnityShaderVariables.cginc>
 
-void Ripple_float (
-float3 PositionIn, float3 Origin,
+void Ripple_float (float3 PositionIn, float3 Origin,
 float Period, float Speed, float Amplitude,
-out float3 PositionOut, out float3 NormalOut, out float3 TangentOut
-) {
+out float3 PositionOut, out float3 NormalOut, out float3 TangentOut)
+{
     float3 p = PositionIn - Origin;
     static const float PI = 3.14159265f;
     float d = length(p);
