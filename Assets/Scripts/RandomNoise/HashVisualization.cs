17c5938e66141571f1285ba14c841cac0c893f68
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-24 16:04:51 +0100

    Update HashVisualization.cs

--- Assets/Scripts/RandomNoise/HashVisualization.cs
+++ Assets/Scripts/RandomNoise/HashVisualization.cs
@@ -22,23 +22,59 @@ namespace RandomNoise
             }
             
         }
-        
+
         static int hashesId = Shader.PropertyToID("_Hashes"),
             configId = Shader.PropertyToID("_Config");
 
         [SerializeField]
         Mesh instanceMesh;
-
         [SerializeField]
         Material material;
-
         [SerializeField, Range(1, 512)]
         int resolution = 16;
 
         NativeArray<uint> hashes;
-
         ComputeBuffer hashesBuffer;
-
         MaterialPropertyBlock propertyBlock;
+
+        void OnEnable () 
+        {
+            int length = resolution * resolution;
+            hashes = new NativeArray<uint>(length, Allocator.Persistent);
+            hashesBuffer = new ComputeBuffer(length, 4);
+
+            new HashJob 
+            {
+                hashes = hashes
+            }.ScheduleParallel(hashes.Length, resolution, default).Complete();
+
+            hashesBuffer.SetData(hashes);
+
+            propertyBlock ??= new MaterialPropertyBlock();
+            propertyBlock.SetBuffer(hashesId, hashesBuffer);
+            propertyBlock.SetVector(configId, new Vector4(resolution, 1f / resolution));
+        }
+        
+        void OnDisable () 
+        {
+            hashes.Dispose();
+            hashesBuffer.Release();
+            hashesBuffer = null;
+        }
+
+        void OnValidate () 
+        {
+            if (hashesBuffer != null && enabled)
+            {
+                OnDisable();
+                OnEnable();
+            }
+        }
+        void Update () 
+        {
+            Graphics.DrawMeshInstancedProcedural
+            (instanceMesh, 0, material, new Bounds(Vector3.zero, Vector3.one),
+                hashes.Length, propertyBlock);
+        }
     }
 }
