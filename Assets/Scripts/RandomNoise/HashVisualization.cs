ebe5711886074692438114e3d4d3e6859c27eeab
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-24 15:55:10 +0100

    update

---
+++ Assets/Scripts/RandomNoise/HashVisualization.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace RandomNoise
+{
+    public class HashVisualization : MonoBehaviour
+    {
+        [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+        struct HashJob : IJobFor
+        {
+
+            [WriteOnly] 
+            public NativeArray<uint> hashes;
+
+            public void Execute(int i)
+            {
+                hashes[i] = (uint)i;
+            }
+        }
+    }
+}
