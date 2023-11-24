170aed485968a48fcf52b0f28a385104cfb582cf
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-24 16:58:18 +0100

    update

---
+++ Assets/Scripts/CustomGraph/Graph.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace CustomGraph
+{
+    public class Graph : MonoBehaviour
+    {
+        [SerializeField]
+        Transform pointPrefab;
+
+        [SerializeField, Range(10,200)] 
+        private int resolution;
+        
+        
+        private void Awake()
+        {
+          var point =  Instantiate(pointPrefab);
+          var position = Vector3.zero;
+          var scale = Vector3.one / 5f;
+
+          for (int i = 0; i < resolution; i++)
+          {
+              position.x = (i + 0.5f) / 5f - 1f;
+              position.y = position.x;
+              point.localPosition = position;
+              point.localScale = scale;
+          }
+        }
+    }
+}
