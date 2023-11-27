d35c76728243bfbf0b3dd8495016f3e6f58e03f6
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-27 18:07:27 +0100

    Update FunctionLibrary.cs

--- Assets/Scripts/CustomGraph/FunctionLibrary.cs
+++ Assets/Scripts/CustomGraph/FunctionLibrary.cs
@@ -6,11 +6,10 @@ namespace CustomGraph
 {
     public static class FunctionLibrary
     {
-        //public delegate float Function(float x, float z, float t);
         public delegate Vector3 Function (float u, float v, float t);
 
-        private static Function[] functions = {Wave, MultiWave, Ripple};
-        public enum FunctionName {Wave, MultiWave, Ripple}
+        private static Function[] functions = {Wave, MultiWave, Ripple, Sphere, Torus};
+        public enum FunctionName {Wave, MultiWave, Ripple, Sphere, Torus}
 
         public static Function GetFunction(FunctionName index)
         {
@@ -39,6 +38,30 @@ namespace CustomGraph
             return p;
         }
 
+        private static Vector3 Sphere (float u, float v, float t) 
+        {
+            float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+            var s = r * Cos(0.5f * PI * v);
+            
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r * Sin(0.5f * PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
+        }
+
+        private static Vector3 Torus (float u, float v, float t) 
+        {
+            float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+            float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+            float s = r1 + r2 * Cos(PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r2 * Sin(PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
+        }
+        
         private static Vector3 Ripple (float u, float v, float t) 
         {
             float d = Sqrt(u * u + v * v);
