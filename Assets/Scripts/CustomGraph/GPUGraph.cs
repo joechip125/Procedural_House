61cbd12cf6b8ae1defdfc3f3b68ae09cfeb6ef5f
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-28 16:25:42 +0100

    Update GPUGraph.cs

--- Assets/Scripts/CustomGraph/GPUGraph.cs
+++ Assets/Scripts/CustomGraph/GPUGraph.cs
@@ -1,9 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace CustomGraph
 {
     public class GPUGraph : MonoBehaviour
     {
+       
+
+        [SerializeField, Range(10,200)] 
+        private int resolution;
+
+        [SerializeField] 
+        private FunctionLibrary.FunctionName functionName;
+
+        private ComputeBuffer positionBuffer;
         
+        private void Update()
+        {
+            var time = Time.time;
+            var step = 2f / resolution;
+            float v = 0.5f * step - 1f;
+            FunctionLibrary.Function f = FunctionLibrary.GetFunction(functionName);
+        }
+
+        private void OnDisable()
+        {
+            positionBuffer.Release();
+            positionBuffer = null;
+        }
+
+        private void OnEnable()
+        {
+            positionBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+        }
     }
 }
