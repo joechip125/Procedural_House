1f4038c8215242a20e2abf6fe6ccaaa2be907e0b
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-28 15:51:33 +0100

    update

---
+++ Assets/Scripts/FrameRateCounter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FrameRateCounter : MonoBehaviour
+{
+    [SerializeField] 
+    private TextMeshProUGUI textDisplay;
+
+    private int frames;
+    private float duration;
+
+    private void Update()
+    {
+        var frameDuration = Time.unscaledDeltaTime;
+        frames += 1;
+        duration += frameDuration;
+        textDisplay.SetText($"FPS\n{1/frameDuration:0}");
+    }
+}
