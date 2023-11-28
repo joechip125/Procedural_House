99a5f0d8467f8850246c9bc7adf47655cb0f9071
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-28 16:18:13 +0100

    update

--- Assets/Scripts/FrameRateCounter.cs
+++ Assets/Scripts/FrameRateCounter.cs
@@ -8,15 +8,32 @@ public class FrameRateCounter : MonoBehaviour
 {
     [SerializeField] 
     private TextMeshProUGUI textDisplay;
+    
+    [SerializeField, Range(0.1f, 2f)]
+    float sampleDuration = 1f;
 
     private int frames;
-    private float duration;
+    private float duration, bestDuration = float.MaxValue, worstDuration;
 
     private void Update()
     {
         var frameDuration = Time.unscaledDeltaTime;
         frames += 1;
         duration += frameDuration;
-        textDisplay.SetText($"FPS\n{1/frameDuration:0}");
+        
+        if (frameDuration < bestDuration) 
+        {
+            bestDuration = frameDuration;
+        }
+        if (frameDuration > worstDuration) 
+        {
+            worstDuration = frameDuration;
+        }
+        if (duration >= sampleDuration)
+        {
+            textDisplay.SetText($"FPS\n{frames/duration:0}\n{1f/bestDuration:0}\n{1f/worstDuration:0}");
+            frames = 0;
+            duration = 0f;
+        }
     }
 }
