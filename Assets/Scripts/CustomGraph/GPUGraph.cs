399e114ceabd4c2141eb46a7328034feb0a44c46
Mattias Lönnqvist <joechip125@gmail.com>
2023-11-29 17:03:59 +0100

    update

--- Assets/Scripts/CustomGraph/GPUGraph.cs
+++ Assets/Scripts/CustomGraph/GPUGraph.cs
@@ -39,7 +39,7 @@ namespace CustomGraph
             computeShader.Dispatch(0, groups, groups, 1);
             
             var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
-            Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, 1);
+            Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionBuffer.count);
         }
         
         private void Update()
