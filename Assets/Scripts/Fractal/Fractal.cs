f8596f7c6c7017956c62d0d95e54fb856ab4b86a
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-05 16:37:24 +0100

    update

--- Assets/Scripts/Fractal/Fractal.cs
+++ Assets/Scripts/Fractal/Fractal.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 namespace Fractal
 {
@@ -12,8 +14,17 @@ namespace Fractal
         {
             name = $"Fractal {depth}";
             if (depth < 1) return;
-            var child = Instantiate(this);
+            
+            var child = Instantiate(this, transform,false);
+            child.transform.localPosition = 0.75f * Vector3.right;
+            child.transform.localScale = 0.5f * Vector3.one;
             child.depth = -1;
+            
+            child = Instantiate(this);
+            child.depth = depth - 1;
+            child.transform.SetParent(transform, false);
+            child.transform.localPosition = 0.75f * Vector3.up;
+            child.transform.localScale = 0.5f* Vector3.one;
         }
     }
 }
