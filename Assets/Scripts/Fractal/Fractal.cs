14f00ec0f85b3bc16dcb8438cd33d797e908c333
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-06 16:48:30 +0100

    Update Fractal.cs

--- Assets/Scripts/Fractal/Fractal.cs
+++ Assets/Scripts/Fractal/Fractal.cs
@@ -27,31 +27,6 @@ namespace Fractal
 
         private FractalPart[][] parts;
 
-        private void Awake()
-        {
-            parts = new FractalPart[depth][];
-            parts[0] = new FractalPart[1];
-            
-            for (int i = 0, length = 1; i < parts.Length; i++, length *= 5) 
-            {
-                parts[i] = new FractalPart[length];
-            }
-
-            var scale = 1f;
-            CreatePart(0,0, scale);
-            for (int li = 1; li < parts.Length; li++)
-            {
-                var levelParts = parts[li];
-                for (int fpi = 0; fpi < levelParts.Length; fpi += 5)
-                {
-                    for (int c = 0; c < 5; c++)
-                    {
-                        CreatePart(li,c, scale);
-                    }
-                }
-            }
-        }
-
         static Vector3[] directions = 
         {
             Vector3.up, Vector3.right, Vector3.left, Vector3.forward, Vector3.back
@@ -63,14 +38,58 @@ namespace Fractal
             Quaternion.Euler(0f, 0f, -90f), Quaternion.Euler(0f, 0f, 90f),
             Quaternion.Euler(90f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f)
         };
+        
+        void Update () 
+        {
+            for (int li = 1; li < parts.Length; li++) 
+            {
+                FractalPart[] levelParts = parts[li];
+                for (int fpi = 0; fpi < levelParts.Length; fpi++) 
+                {
+                    FractalPart part = levelParts[fpi];
+                }
+            }
+        }
 
-        private void CreatePart(int levelIndex, int childIndex, float scale)
+        private void Awake()
+        {
+            parts = new FractalPart[depth][];
+            parts[0] = new FractalPart[1];
+            
+            for (int i = 0, length = 1; i < parts.Length; i++, length *= 5) 
+            {
+                parts[i] = new FractalPart[length];
+            }
+
+            var scale = 1f;
+            parts[0][0] = CreatePart(0,0, scale);
+            for (int li = 1; li < parts.Length; li++)
+            {
+                scale *= 0.5f;
+                var levelParts = parts[li];
+                for (int fpi = 0; fpi < levelParts.Length; fpi += 5)
+                {
+                    for (int c = 0; c < 5; c++)
+                    {
+                        levelParts[fpi + c]=CreatePart(li,c, scale);
+                    }
+                }
+            }
+        }
+
+        private FractalPart CreatePart(int levelIndex, int childIndex, float scale)
         {
             var go = new GameObject($"Fractal part L{levelIndex} C{childIndex}");
             go.transform.SetParent(transform, false);
             go.transform.localScale = Vector3.one * scale; 
             go.AddComponent<MeshFilter>().mesh = mesh;
             go.AddComponent<MeshRenderer>().material = material;
+            return new FractalPart()
+            {
+                direction = directions[childIndex],
+                rotation = rotations[childIndex],
+                transform = go.transform
+            };
         }
         
         private Fractal CreateChild(Vector3 direction, Quaternion rotation)
