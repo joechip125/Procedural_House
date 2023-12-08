2bd488d721df7917909cb7400b99219d21af6981
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-08 18:26:40 +0100

    Update FractalGPU.hlsl

--- Assets/Scripts/Fractal/FractalGPU.hlsl
+++ Assets/Scripts/Fractal/FractalGPU.hlsl
@@ -1,17 +1,11 @@
 ﻿#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
-StructuredBuffer<float3> _Positions;
+StructuredBuffer<float4x4> _Matrices;
 #endif
-
-float _Step;
 
 void ConfigureProcedural ()
 {
     #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
-    float3 position = _Positions[unity_InstanceID];
-
-    unity_ObjectToWorld = 0.0;
-    unity_ObjectToWorld._m03_m13_m23_m33 = float4(position, 1.0);
-    unity_ObjectToWorld._m00_m11_m22 = _Step;
+    unity_ObjectToWorld = _Matrices[unity_InstanceID];
     #endif
 }
 
