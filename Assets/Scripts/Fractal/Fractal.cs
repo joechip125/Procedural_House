5cfdbfb8481f7e031a25deff0b83e252830797c1
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-11 14:40:28 +0100

    Update Fractal.cs

--- Assets/Scripts/Fractal/Fractal.cs
+++ Assets/Scripts/Fractal/Fractal.cs
@@ -1,16 +1,50 @@
-﻿using System;
-using System.Numerics;
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
 using Unity.Mathematics;
-using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
-using Matrix4x4 = UnityEngine.Matrix4x4;
-using Quaternion = UnityEngine.Quaternion;
-using Vector3 = UnityEngine.Vector3;
+
+using static Unity.Mathematics.math;
+using quaternion = Unity.Mathematics.quaternion;
 
 namespace Fractal
 {
     public class Fractal : MonoBehaviour
     {
+        
+        [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+        struct UpdateFractalLevelJob : IJobFor 
+        {
+
+            public float spinAngleDelta;
+            public float scale;
+
+            [ReadOnly]
+            public NativeArray<FractalPart> parents;
+
+            public NativeArray<FractalPart> parts;
+
+            [WriteOnly]
+            public NativeArray<float3x4> matrices;
+
+            public void Execute (int i) {
+                FractalPart parent = parents[i / 5];
+                FractalPart part = parts[i];
+                part.spinAngle += spinAngleDelta;
+                part.worldRotation = mul(parent.worldRotation,
+                    mul(part.rotation, quaternion.RotateY(part.spinAngle))
+                );
+                part.worldPosition =
+                    parent.worldPosition +
+                    mul(parent.worldRotation, 1.5f * scale * part.direction);
+                parts[i] = part;
+
+                float3x3 r = float3x3(part.worldRotation) * scale;
+                matrices[i] = float3x4(r.c0, r.c1, r.c2, part.worldPosition);
+            }
+        }
+
+        
         private struct FractalPart
         {
             public Vector3 direction, worldPosition;
