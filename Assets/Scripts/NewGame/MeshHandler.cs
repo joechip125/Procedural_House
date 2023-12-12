503a863ea33cbaeab957be9138da93b50f1c6123
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-12 17:07:29 +0100

    Update MeshHandler.cs

--- Assets/Scripts/NewGame/MeshHandler.cs
+++ Assets/Scripts/NewGame/MeshHandler.cs
@@ -1,6 +1,15 @@
-﻿using UnityEngine;
+﻿using Unity.Mathematics;
+using UnityEngine;
 
 public class MeshHandler : MonoBehaviour
 {
-        
+    [SerializeField] 
+    private Mesh mesh;
+    
+    private struct MeshPart
+    {
+        public float3 worldPosition;
+        public Quaternion rotation;
+    }
+    
 }
