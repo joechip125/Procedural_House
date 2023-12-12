2d5295124996c9fcc37525a6d8ed867fbdf1033b
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-12 17:09:46 +0100

    update

--- Assets/Scripts/NewGame/MeshHandler.cs
+++ Assets/Scripts/NewGame/MeshHandler.cs
@@ -6,6 +6,9 @@ public class MeshHandler : MonoBehaviour
     [SerializeField] 
     private Mesh mesh;
     
+    [SerializeField] 
+    private Material material;
+    
     private struct MeshPart
     {
         public float3 worldPosition;
