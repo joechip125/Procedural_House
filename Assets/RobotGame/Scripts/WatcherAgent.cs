86066c0e54734024dcff2160989a7a48514e63f3
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-15 15:27:45 +0100

    update

--- Assets/RobotGame/Scripts/WatcherAgent.cs
+++ Assets/RobotGame/Scripts/WatcherAgent.cs
@@ -1,9 +1,34 @@
 ﻿using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 namespace RobotGame.Scripts
 {
     public class WatcherAgent : Agent
     {
-        
+        public override void Initialize()
+        {
+            
+        }
+
+        public override void CollectObservations(VectorSensor sensor)
+        {
+            
+        }
+
+        public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask)
+        {
+            
+        }
+
+        public override void OnActionReceived(ActionBuffers actions)
+        {
+            
+        }
+
+        public override void OnEpisodeBegin()
+        {
+           
+        }
     }
 }
