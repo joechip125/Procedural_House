5bfdeeab28adfeab026e3732487154f3a3f68c9b
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-20 13:00:29 +0100

    update

---
+++ Assets/RobotGame/Scripts/Gun.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace RobotGame.Scripts
+{
+    public class Gun : MonoBehaviour, IComponent
+    {
+
+        [SerializeField] 
+        private Transform exit;
+        
+        public void Use()
+        {
+            
+        }
+    }
+}
