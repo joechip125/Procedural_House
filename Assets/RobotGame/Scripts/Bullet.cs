d2dbf1eb8afdfc4e3cdd38da4a922e1dcccca90e
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-22 17:07:02 +0100

    Update Bullet.cs

--- Assets/RobotGame/Scripts/Bullet.cs
+++ Assets/RobotGame/Scripts/Bullet.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace RobotGame.Scripts
 {
     public class Bullet : MonoBehaviour
     {
-        
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (!collision.gameObject.CompareTag("SimpleTarget")) return;
+            
+        }
     }
 }
