fe891395bffe8fc987254edb42cc213ccd57ab4d
Mattias Lönnqvist <joechip125@gmail.com>
2023-12-27 18:43:40 +0100

    update

--- Assets/PersistentObjects/Scripts/Game.cs
+++ Assets/PersistentObjects/Scripts/Game.cs
@@ -1,9 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace PersistentObjects.Scripts
 {
     public class Game : MonoBehaviour
     {
-        
+        public Transform prefab;
+
+        public KeyCode createKey = KeyCode.C;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(createKey))
+            {
+                CreateObject();
+            }
+        }
+
+        private void CreateObject()
+        {
+            Instantiate(prefab, transform);
+        }
     }
 }
