22ea9d30b14ed3db1f5aaf7c62e0826df02c2ddd
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-01 14:19:18 +0100

    update

--- Assets/PersistentObjects/Scripts/GameDataWriter.cs
+++ Assets/PersistentObjects/Scripts/GameDataWriter.cs
@@ -1,9 +1,41 @@
 ﻿using System.IO;
+using UnityEngine;
 
 namespace PersistentObjects.Scripts
 {
     public class GameDataWriter
     {
+
+        public GameDataWriter(BinaryWriter binaryWriter)
+        {
+            
+        }
+        
+        public void Write (float value) 
+        {
+            writer.Write(value);
+        }
+
+        public void Write (int value) 
+        {
+            writer.Write(value);
+        }
+        
+        public void Write (Quaternion value) 
+        {
+            writer.Write(value.x);
+            writer.Write(value.y);
+            writer.Write(value.z);
+            writer.Write(value.w);
+        }
+	
+        public void Write (Vector3 value) 
+        {
+            writer.Write(value.x);
+            writer.Write(value.y);
+            writer.Write(value.z);
+        }
+        
         private BinaryWriter writer;
     }
 }
