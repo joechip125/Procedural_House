8022cbc7a812a2e2b4daa99827df7c164e9855ee
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-01 16:15:44 +0100

    Update Shape.cs

--- Assets/PersistentObjects/Scripts/Shape.cs
+++ Assets/PersistentObjects/Scripts/Shape.cs
@@ -4,6 +4,20 @@ namespace PersistentObjects.Scripts
 {
     public class Shape : PersistableObject
     {
+        public int ShapeID
+        {
+            get => shapeID;
+            set
+            {
+                if(shapeID == int.MinValue && value != int.MinValue) shapeID = value;
+                else
+                {
+                    Debug.LogError("Not allowed to change shapeId.");
+                }
+            }
+            
+        }
         
+        private int shapeID;
     }
 }
