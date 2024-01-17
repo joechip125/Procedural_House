34155d4ac71398da4453c557562e5a56c24bbaaf
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-17 15:58:47 +0100

    Update SpaceTRS.cs

--- Assets/Pseudo Random Noise/Scripts/SpaceTRS.cs
+++ Assets/Pseudo Random Noise/Scripts/SpaceTRS.cs
@@ -8,11 +8,12 @@ namespace RandomNoise
     {
         public float3 rotation, translation, scale;
         
-        public float4x4 Matrix 
+        public float3x4 Matrix 
         {
             get 
             {
-                return float4x4.TRS(translation, quaternion.EulerZXY(math.radians(rotation)), scale);
+                float4x4 m = float4x4.TRS(translation, quaternion.EulerZXY(math.radians(rotation)), scale);
+                return math.float3x4(m.c0.xyz, m.c1.xyz, m.c2.xyz, m.c3.xyz);
             }
         }
     }
