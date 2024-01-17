5dcc42abfaa0dd2d20ad111c0f1cda07db939abe
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-17 15:54:04 +0100

    Update SpaceTRS.cs

--- Assets/Pseudo Random Noise/Scripts/SpaceTRS.cs
+++ Assets/Pseudo Random Noise/Scripts/SpaceTRS.cs
@@ -1,9 +1,19 @@
-﻿using Unity.Mathematics;
+﻿using System;
+using Unity.Mathematics;
 
 namespace RandomNoise
 {
+    [Serializable]
     public struct SpaceTRS
     {
         public float3 rotation, translation, scale;
+        
+        public float4x4 Matrix 
+        {
+            get 
+            {
+                return float4x4.TRS(translation, quaternion.EulerZXY(math.radians(rotation)), scale);
+            }
+        }
     }
 }
