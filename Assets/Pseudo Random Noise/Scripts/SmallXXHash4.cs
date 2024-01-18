41adcd2c71f1df640b4f4df1f842705385059249
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-18 19:43:54 +0100

    Update SmallXXHash4.cs

--- Assets/Pseudo Random Noise/Scripts/SmallXXHash4.cs
+++ Assets/Pseudo Random Noise/Scripts/SmallXXHash4.cs
@@ -1,4 +1,6 @@
-﻿namespace RandomNoise
+﻿using Unity.Mathematics;
+
+namespace RandomNoise
 {
     public readonly struct SmallXXHash
     {
@@ -43,17 +45,16 @@
     
     public readonly struct SmallXXHash4
     {
-        const uint primeA = 0b10011110001101110111100110110001;
         const uint primeB = 0b10000101111010111100101001110111;
         const uint primeC = 0b11000010101100101010111000111101;
         const uint primeD = 0b00100111110101001110101100101111;
         const uint primeE = 0b00010110010101100110011110110001;
         
-        readonly uint accumulator;
+        readonly uint4 accumulator;
 
-        public static implicit operator uint (SmallXXHash4 hash) 
+        public static implicit operator uint4 (SmallXXHash4 hash) 
         {
-            uint avalanche = hash.accumulator;
+            uint4 avalanche = hash.accumulator;
             avalanche ^= avalanche >> 15;
             avalanche *= primeB;
             avalanche ^= avalanche >> 13;
@@ -62,20 +63,18 @@
             return avalanche;
         }
         
-        static uint RotateLeft (uint data, int steps) =>
+        static uint4 RotateLeft (uint4 data, int steps) =>
             (data << steps) | (data >> 32 - steps);
         
-        public SmallXXHash4 Eat (int data) =>
-            RotateLeft(accumulator + (uint)data * primeC, 17) * primeD;
-
-        public SmallXXHash4 Eat (byte data) =>
-            RotateLeft(accumulator + data * primeE, 11) * primeA;
-        public static implicit operator SmallXXHash4 (uint accumulator) =>
+        public SmallXXHash4 Eat (int4 data) =>
+            RotateLeft(accumulator + (uint4)data * primeC, 17) * primeD;
+        
+        public static implicit operator SmallXXHash4 (uint4 accumulator) =>
             new SmallXXHash4(accumulator);
         
-        public static SmallXXHash4 Seed (int seed) => (uint)seed + primeE;
+        public static SmallXXHash4 Seed (int4 seed) => (uint4)seed + primeE;
         
-        public SmallXXHash4 (uint accumulator) 
+        public SmallXXHash4 (uint4 accumulator) 
         {
             this.accumulator = accumulator;
         }
