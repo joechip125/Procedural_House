6fe0bde30bae63ce3b3da5c5a28efccc7fd28dfa
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-18 19:34:03 +0100

    update

--- Assets/Pseudo Random Noise/Scripts/SmallXXHash4.cs
+++ Assets/Pseudo Random Noise/Scripts/SmallXXHash4.cs
@@ -40,4 +40,45 @@
         }
         
     }
+    
+    public readonly struct SmallXXHash4
+    {
+        const uint primeA = 0b10011110001101110111100110110001;
+        const uint primeB = 0b10000101111010111100101001110111;
+        const uint primeC = 0b11000010101100101010111000111101;
+        const uint primeD = 0b00100111110101001110101100101111;
+        const uint primeE = 0b00010110010101100110011110110001;
+        
+        readonly uint accumulator;
+
+        public static implicit operator uint (SmallXXHash4 hash) 
+        {
+            uint avalanche = hash.accumulator;
+            avalanche ^= avalanche >> 15;
+            avalanche *= primeB;
+            avalanche ^= avalanche >> 13;
+            avalanche *= primeC;
+            avalanche ^= avalanche >> 16;
+            return avalanche;
+        }
+        
+        static uint RotateLeft (uint data, int steps) =>
+            (data << steps) | (data >> 32 - steps);
+        
+        public SmallXXHash4 Eat (int data) =>
+            RotateLeft(accumulator + (uint)data * primeC, 17) * primeD;
+
+        public SmallXXHash4 Eat (byte data) =>
+            RotateLeft(accumulator + data * primeE, 11) * primeA;
+        public static implicit operator SmallXXHash4 (uint accumulator) =>
+            new SmallXXHash4(accumulator);
+        
+        public static SmallXXHash4 Seed (int seed) => (uint)seed + primeE;
+        
+        public SmallXXHash4 (uint accumulator) 
+        {
+            this.accumulator = accumulator;
+        }
+        
+    }
 }
