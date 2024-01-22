bfff960644457d3e4dcfa7671c75b13a8ec330b0
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-22 16:02:17 +0100

    Update Shapes.cs

--- Assets/Pseudo Random Noise/Scripts/Shapes.cs
+++ Assets/Pseudo Random Noise/Scripts/Shapes.cs
@@ -28,6 +28,43 @@ namespace RandomNoise
             }
         }
         
+        public struct Sphere : IShape 
+        {
+            public Point4 GetPoint4 (int i, float resolution, float invResolution) 
+            {
+                float4x2 uv = IndexTo4UV(i, resolution, invResolution);
+
+                float r = 0.5f;
+                float4 s = r * sin(PI * uv.c1);
+
+                Point4 p;
+                p.positions.c0 = s * sin(2f * PI * uv.c0);
+                p.positions.c1 = r * cos(PI * uv.c1);
+                p.positions.c2 = s * cos(2f * PI * uv.c0);
+                p.normals = p.positions;
+                return p;
+            }
+        }
+        
+        public struct Torus : IShape 
+        {
+            public Point4 GetPoint4 (int i, float resolution, float invResolution) 
+            {
+                float4x2 uv = IndexTo4UV(i, resolution, invResolution);
+
+                float r1 = 0.375f;
+                float r2 = 0.125f;
+                float4 s = r1 + r2 * cos(2f * PI * uv.c1);
+
+                Point4 p;
+                p.positions.c0 = s * sin(2f * PI * uv.c0);
+                p.positions.c1 = r2 * sin(2f * PI * uv.c1);
+                p.positions.c2 = s * cos(2f * PI * uv.c0);
+                p.normals = p.positions;
+                return p;
+            }
+        }
+        
         public struct Point4 
         {
             public float4x3 positions, normals;
