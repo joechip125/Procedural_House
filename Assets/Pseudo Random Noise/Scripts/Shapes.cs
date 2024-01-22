c7086536b5af03d5da1bb56f0667d83b4e136f42
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-22 13:55:14 +0100

    Update Shapes.cs

--- Assets/Pseudo Random Noise/Scripts/Shapes.cs
+++ Assets/Pseudo Random Noise/Scripts/Shapes.cs
@@ -9,6 +9,21 @@ namespace RandomNoise
 {
     public static class Shapes
     {
+        public struct Point4 
+        {
+            public float4x3 positions, normals;
+        }
+        
+        public static float4x2 IndexTo4UV (int i, float resolution, float invResolution) 
+        {
+            float4x2 uv;
+            float4 i4 = 4f * i + float4(0f, 1f, 2f, 3f);
+            uv.c1 = floor(invResolution * i4 + 0.00001f);
+            uv.c0 = invResolution * (i4 - resolution * uv.c1 + 0.5f);
+            uv.c1 = invResolution * (uv.c1 + 0.5f);
+            return uv;
+        }
+        
         [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
         public struct Job : IJobFor 
         {
