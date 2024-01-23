823b593e8d5b88d63ab25ec456a21dd547b0053b
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-23 15:26:17 +0100

    Update HashVisualization.cs

--- Assets/Pseudo Random Noise/Scripts/HashVisualization.cs
+++ Assets/Pseudo Random Noise/Scripts/HashVisualization.cs
@@ -8,11 +8,12 @@ using static Unity.Mathematics.math;
 
 namespace Pseudo_Random_Noise.Scripts
 {
-	public class HashVisualization : MonoBehaviour 
+	public class HashVisualization : Visualization
 	{
 
 		[BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
-		struct HashJob : IJobFor {
+		struct HashJob : IJobFor 
+		{
 
 			[ReadOnly]
 			public NativeArray<float3x4> positions;
@@ -40,20 +41,9 @@ namespace Pseudo_Random_Noise.Scripts
 				hashes[i] = hash.Eat(u).Eat(v).Eat(w);
 			}
 		}
-
-		public enum Shape { Plane, Sphere, Torus }
-
-		static Shapes.ScheduleDelegate[] shapeJobs = {
-			Shapes.Job<Shapes.Plane>.ScheduleParallel,
-			Shapes.Job<Shapes.Sphere>.ScheduleParallel,
-			Shapes.Job<Shapes.Torus>.ScheduleParallel
-		};
-
-		static int
-			hashesId = Shader.PropertyToID("_Hashes"),
-			positionsId = Shader.PropertyToID("_Positions"),
-			normalsId = Shader.PropertyToID("_Normals"),
-			configId = Shader.PropertyToID("_Config");
+		
+		private static int
+			hashesId = Shader.PropertyToID("_Hashes");
 
 		[SerializeField]
 		Mesh instanceMesh;
@@ -82,50 +72,31 @@ namespace Pseudo_Random_Noise.Scripts
 		};
 
 		NativeArray<uint4> hashes;
+		
+		ComputeBuffer hashesBuffer;
 
-		NativeArray<float3x4> positions, normals;
-
-		ComputeBuffer hashesBuffer, positionsBuffer, normalsBuffer;
-
-		MaterialPropertyBlock propertyBlock;
-
-		bool isDirty;
-
-		Bounds bounds;
-
-		void OnEnable () {
-			isDirty = true;
-
-			int length = resolution * resolution;
-			length = length / 4 + (length & 1);
-			hashes = new NativeArray<uint4>(length, Allocator.Persistent);
-			positions = new NativeArray<float3x4>(length, Allocator.Persistent);
-			normals = new NativeArray<float3x4>(length, Allocator.Persistent);
-			hashesBuffer = new ComputeBuffer(length * 4, 4);
-			positionsBuffer = new ComputeBuffer(length * 4, 3 * 4);
-			normalsBuffer = new ComputeBuffer(length * 4, 3 * 4);
-
-			propertyBlock ??= new MaterialPropertyBlock();
+		
+		protected override void EnableVisualization(int dataLength, MaterialPropertyBlock propertyBlock)
+		{
+			hashes = new NativeArray<uint4>(dataLength, Allocator.Persistent);
+			hashesBuffer = new ComputeBuffer(dataLength * 4,4);
 			propertyBlock.SetBuffer(hashesId, hashesBuffer);
-			propertyBlock.SetBuffer(positionsId, positionsBuffer);
-			propertyBlock.SetBuffer(normalsId, normalsBuffer);
-			propertyBlock.SetVector(configId, new Vector4(
-				resolution, instanceScale / resolution, displacement
-			));
 		}
 
-		void OnDisable () {
+		protected override void DisableVisualization()
+		{
 			hashes.Dispose();
-			positions.Dispose();
-			normals.Dispose();
 			hashesBuffer.Release();
-			positionsBuffer.Release();
-			normalsBuffer.Release();
 			hashesBuffer = null;
-			positionsBuffer = null;
-			normalsBuffer = null;
 		}
 
+		protected override void UpdateVisualization(NativeArray<float3x4> positions, int resolution, JobHandle handle)
+		{
+			throw new System.NotImplementedException();
+		}
+
+	
+		
 		void OnValidate () {
 			if (hashesBuffer != null && enabled) {
 				OnDisable();
