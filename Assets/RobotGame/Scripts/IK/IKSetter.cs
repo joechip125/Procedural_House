001930410eee825c0927efdc4fd77017599064da
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-24 18:39:00 +0100

    Update IKSetter.cs

--- Assets/RobotGame/Scripts/IK/IKSetter.cs
+++ Assets/RobotGame/Scripts/IK/IKSetter.cs
@@ -24,28 +24,11 @@ namespace RobotGame.Scripts.IK
             quit = false;
             targets = new List<Transform>();
 
-            var child = transform.GetChild(0);
-            
-            var current = transform;
-            while (!quit)
-            {
-                if (current != null)
-                {
-                    var children = current
-                        .Cast<Transform>()
-                        .ToArray();
-                    
-
-                    foreach (var c in children)
-                    {
-                       
-                    }
-                    
-
-                    child = current.GetChild(0);
-                }
-                quit = true;
-            }
+            var children = transform
+                .Cast<Transform>()
+                .ToArray()
+                .Where(x => nameOfLeafNodes
+                    .Contains(x.name));
         }
     }
 }
