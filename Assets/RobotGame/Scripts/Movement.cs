d19385ac06d1c4a1fc2bbafc855d0dc1d7727686
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-24 14:51:16 +0100

    update

---
+++ Assets/RobotGame/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace RobotGame.Scripts
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] 
+        private KeyCode forward = KeyCode.W;
+        [SerializeField] 
+        private KeyCode back = KeyCode.S;
+        [SerializeField] 
+        private KeyCode left= KeyCode.A;
+        [SerializeField] 
+        private KeyCode right= KeyCode.D;
+
+        [SerializeField] private float speed = 4f;
+        
+        private void Update()
+        {
+            var moveAmount = Vector3.zero;
+            
+            if (Input.GetKey(forward))
+            {
+                moveAmount += Vector3.forward * (Time.deltaTime * speed);
+            }
+            else if (Input.GetKey(back))
+            {
+                moveAmount += Vector3.back * (Time.deltaTime * speed);
+            }
+            
+            if (Input.GetKey(left))
+            {
+                moveAmount += Vector3.left * (Time.deltaTime * speed);
+            }
+            else if (Input.GetKey(right))
+            {
+                moveAmount += Vector3.right * (Time.deltaTime * speed);
+            }
+
+            transform.position += moveAmount;
+        }
+    }
+}
