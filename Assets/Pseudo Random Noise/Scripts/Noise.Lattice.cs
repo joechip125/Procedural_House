e270e64a4a9111d93e3149e01cde0732dcc77626
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-24 14:17:38 +0100

    update

--- Assets/Pseudo Random Noise/Scripts/Noise.Lattice.cs
+++ Assets/Pseudo Random Noise/Scripts/Noise.Lattice.cs
@@ -6,9 +6,23 @@ namespace Pseudo_Random_Noise.Scripts
 {
     public static partial class Noise
     {
+        struct LatticeSpan4 
+        {
+            public int4 p0, p1;
+            public float4 t;
+        }
+        static LatticeSpan4 GetLatticeSpan4 (float4 coordinates) 
+        {
+            float4 points = floor(coordinates);
+            LatticeSpan4 span;
+            span.p0 = (int4)points;
+            span.p1 = span.p0 + 1;
+            span.t = coordinates - points;
+            return span;
+        }
+        
         public struct Lattice1D : INoise 
         {
-
             public float4 GetNoise4(float4x3 positions, SmallXXHash4 hash) 
             {
                 int4 p0 = (int4)floor(positions.c0);
