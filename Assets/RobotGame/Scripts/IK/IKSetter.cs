dadc0125d20a4780d70a6859de1fa7425ec57910
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-24 18:19:26 +0100

    update

---
+++ Assets/RobotGame/Scripts/IK/IKSetter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace RobotGame.Scripts.IK
+{
+    public class IKSetter : MonoBehaviour
+    {
+        private List<Transform> targets;
+        //Name
+        //Transform of handle
+        private bool quit;
+        private const string rootName = "Root";
+
+        [SerializeField] 
+        private List<string> nameOfLeafNodes;
+        
+        [SerializeField] 
+        private GameObject handle;
+        
+        private void Start()
+        {
+            quit = false;
+            targets = new List<Transform>();
+
+            var child = transform.GetChild(0);
+            
+            var current = transform;
+            while (!quit)
+            {
+                if (current != null)
+                {
+                    var numChild = current.childCount;
+
+                    if (numChild < 2)
+                    {
+                        
+                        continue;
+                    }
+                    
+                    var children = current
+                        .Cast<Transform>()
+                        .ToArray();
+                    
+                    foreach (var c in children)
+                    {
+                       
+                    }
+
+                    for (int i = 0; i < numChild; i++)
+                    { 
+                        if (child.childCount > 1)
+                        {
+                           
+                        }
+                    }
+
+                    child = current.GetChild(0);
+                }
+                quit = true;
+            }
+        }
+    }
+}
