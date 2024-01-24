97cabfd3add24bc5d232c4d7412efc28406b42cb
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-24 14:19:16 +0100

    Update Noise.Lattice.cs

--- Assets/Pseudo Random Noise/Scripts/Noise.Lattice.cs
+++ Assets/Pseudo Random Noise/Scripts/Noise.Lattice.cs
@@ -25,10 +25,8 @@ namespace Pseudo_Random_Noise.Scripts
         {
             public float4 GetNoise4(float4x3 positions, SmallXXHash4 hash) 
             {
-                int4 p0 = (int4)floor(positions.c0);
-                int4 p1 = p0 + 1;
-                float4 t = positions.c0 - p0;
-                return lerp(hash.Eat(p0).Floats01A, hash.Eat(p1).Floats01A, t) * 2f - 1f;
+                LatticeSpan4 x = GetLatticeSpan4(positions.c0);
+                return lerp(hash.Eat(x.p0).Floats01A, hash.Eat(x.p1).Floats01A, x.t) * 2f - 1f;
             }
         }
     }
