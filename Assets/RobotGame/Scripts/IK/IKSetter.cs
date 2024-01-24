64374273497286caf307a0fd75f3afa76564558d
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-24 19:20:14 +0100

    update

--- Assets/RobotGame/Scripts/IK/IKSetter.cs
+++ Assets/RobotGame/Scripts/IK/IKSetter.cs
@@ -5,31 +5,51 @@ using UnityEngine;
 
 namespace RobotGame.Scripts.IK
 {
+
+    [Serializable]
+    public struct ControlNode
+    {
+        public ControlNode(string theName)
+        {
+            
+        }
+    }
+
     public class IKSetter : MonoBehaviour
     {
+        [SerializeField]
         private List<Transform> leafNodes;
         //Name
         //Transform of handle
         private bool quit;
         private const string rootName = "Root";
-
-        [SerializeField] 
-        private List<string> nameOfLeafNodes;
         
         [SerializeField] 
         private GameObject handle;
         
         private void Start()
         {
-            quit = false;
-            leafNodes = new List<Transform>();
+          
+        }
 
-            leafNodes = transform
+        private void SetLeafNodes()
+        {
+            var nodes = transform
                 .Cast<Transform>()
-                .ToArray()
-                .Where(x => nameOfLeafNodes
-                    .Contains(x.name))
-                .ToList();
+                .ToArray().ToList();
+            //.Where(x => nameOfLeafNodes
+            //    .Contains(x.name))
+            //.ToList();
+        }
+        
+        private void OnDrawGizmos()
+        {
+            if (!Application.isPlaying) return;
+            
+            for (int i = 0; i < leafNodes.Count; i++)
+            {
+                Gizmos.DrawWireSphere(leafNodes[i].position, 0.3f);
+            }
         }
     }
 }
