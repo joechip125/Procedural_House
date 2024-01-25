f8eaffc1371fa91bd98a3a9fd0ecf238cc2688e8
Mattias Lönnqvist <joechip125@gmail.com>
2024-01-25 22:39:49 +0100

    update

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-public class Grid : MonoBehaviour {
+public class Grid : MonoBehaviour 
+{
 
 	public int xSize, ySize;
 
 	private Mesh mesh;
 	private Vector3[] vertices;
 
-	private void Awake () {
+	private void Awake () 
+	{
 		Generate();
 	}
 
